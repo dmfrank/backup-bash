36ff3844b81a36b18e9bb0bf0dc928a7767d4393
Dmitry V.Frank <dmitriyfrank@gmail.com>
2018-02-27 15:45:57 +0700

    moving forward

--- backup.sh
+++ backup.sh
@@ -1,34 +1,109 @@
 #!/bin/bash
 
-## vars
 PREFIX=`date +%FT%T`
-CONFIG_DIR=./conf/
-CONFIG=$CONFIG_DIR/config
-TARGET=$CONFIG_DIR/options
+BASE_DIR=.
+CONFIG=$BASE_DIR/config
+BKP_DIR=$BASE_DIR/bkp
 
-# read backup target options congiguration file
-. $TARGET 
-
-function backmeup {
-	for f in $(find $TARGET_BKP_DIRNAME -name '*'); 
-		do grep -q -F $f $CONFIG_DIR/toc || echo $f > $CONFIG_DIR/toc; 
-	done
-}
-
-function read_option {
-	cat $CONFIG | while IFS='' read -r line; 
-	do 
+function run {
+	# verify backup directory existing
+	if [ ! -d "$BKP_DIR" ]; then
+		# goto backup directory
+		mkdir -p $BKP_DIR
+	fi
+	cat $CONFIG | while IFS='' read -r line; do 
+		# Check whether satisfy line to pattern or not
+		# skip commented lines
 		[[ "$line" =~ ^#.*$ ]] || [[ -z "$line" ]] && continue
-		for o in $line
-		do
-			if [[ $o == *M ]] || [[ $o == *G ]] || [[  $o == *k ]] || [[ $o == *c ]]
-			then
-				litera='${o: -1}'
-				size='${o: 1: -2}'
+		local cnt=0
+		for o in $line;	do
+			# fetch size options
+			if [[ $cnt == 0 ]]; then 
+				if [[ $o =~ ^\[\+|\-|=&&[0-9]\] ]]; then
+					local litera="${o: -1}"
+					local signSz="${o: 1: -2}"
+					local prefix="${signSz: 0: 1}"
+					local numSz="${signSz: 1}"
+					local pattern="${o}"
+				fi
 			fi
+			# fetch file mask
+			if [[ $cnt == 1 ]]; then
+				local filemask="${o}"
+			fi
+			# fetch target
+			if [[ $cnt == 2 ]]; then
+				local dir="${o}"
+			fi
+			
+			# loop over target directory
+			#IFS=$'\n'; set -f
+			
+			for f in $(find $dir -name "$filemask"); do	
+				# cut current dir symbol
+				local filepath="${f: 1}"
+				# fetching filesize 
+				fsize=$(stat -c%s "$f")
+			
+				# verify on existing dir in $BKP_DIR
+				if [[ ! -d "$(dirname "$BKP_DIR$filepath")" ]]; then
+					mkdir -p "$(dirname "$BKP_DIR$filepath")"
+				fi
+
+				# verify on existing file in $BKP_DIR
+				if [[ ! -d "$BKP_DIR$filepath" ]]; then
+					echo "$BKP_DIR$filepath"
+				#	touch "$BKP_DIR$filepath"
+				fi
+
+				local matched=false
+			
+				case $prefix in
+					-)
+						if [[ "$fsize" -lt "$numSz" ]]; then
+							matched=true							
+						fi
+						;;
+					+)
+						if [[ "$fsize" -gt "$numSz" ]]; then
+							matched=true						
+						fi
+						;;
+					=)
+						if [[ "$fsize" -eq "$numSz" ]]; then
+							matched=true
+						fi
+						;;
+				esac
+
+				if [[ "$matched" = true ]]; then
+					#diff "$f" "$BKP_DIR$filepath" > "$BKP_DIR$filepath".$(date +%s).patch
+					echo 0
+				fi
+
+
+
+
+
+
+
+				
+
+			done
+			#unset IFS; set +f
+
+
+
+
+
+			((cnt++))
 		done
 	done
+	echo "#### Done ####"
 }
 
-read_option
-echo $litera
+while true; do
+	# tick for execution
+	sleep 2
+	run
+done
