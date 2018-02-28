4f58e59fd29c71904716d820d20c0db6f8fd7af9
Dmitry Frank <barak.vs.obama@gmail.com>
2018-02-28 07:52:59 +0700

    Mac bash changes

--- backup.sh
+++ backup.sh
@@ -20,11 +20,10 @@ function run {
 			# fetch size options
 			if [[ $cnt == 0 ]]; then 
 				if [[ $o =~ ^\[\+|\-|=&&[0-9]\] ]]; then
-					local litera="${o: -1}"
-					local signSz="${o: 1: -2}"
-					local prefix="${signSz: 0: 1}"
-					local numSz="${signSz: 1}"
-					local pattern="${o}"
+					local litera="${o: ${#o} - 1}"
+					local signSz="${o: 1: ${#o} - 3}"
+					local prefix="${o: 1: 1}"
+					local numSz="${signSz: 2: ${#signSz} - 3}"
 				fi
 			fi
 			# fetch file mask
@@ -35,69 +34,56 @@ function run {
 			if [[ $cnt == 2 ]]; then
 				local dir="${o}"
 			fi
-			
-			# loop over target directory
-			#IFS=$'\n'; set -f
-			
-			for f in $(find $dir -name "$filemask"); do	
-				# cut current dir symbol
-				local filepath="${f: 1}"
-				# fetching filesize 
-				fsize=$(stat -c%s "$f")
-			
-				# verify on existing dir in $BKP_DIR
-				if [[ ! -d "$(dirname "$BKP_DIR$filepath")" ]]; then
-					mkdir -p "$(dirname "$BKP_DIR$filepath")"
-				fi
-
-				# verify on existing file in $BKP_DIR
-				if [[ ! -d "$BKP_DIR$filepath" ]]; then
-					echo "$BKP_DIR$filepath"
-				#	touch "$BKP_DIR$filepath"
-				fi
-
-				local matched=false
-			
-				case $prefix in
-					-)
-						if [[ "$fsize" -lt "$numSz" ]]; then
-							matched=true							
-						fi
-						;;
-					+)
-						if [[ "$fsize" -gt "$numSz" ]]; then
-							matched=true						
-						fi
-						;;
-					=)
-						if [[ "$fsize" -eq "$numSz" ]]; then
-							matched=true
-						fi
-						;;
-				esac
-
-				if [[ "$matched" = true ]]; then
-					#diff "$f" "$BKP_DIR$filepath" > "$BKP_DIR$filepath".$(date +%s).patch
-					echo 0
-				fi
-
-
-
-
-
-
-
-				
-
-			done
-			#unset IFS; set +f
-
-
-
-
-
 			((cnt++))
 		done
+		# loop over target directory
+		#IFS=$'\n'; set -f
+		
+		for f in $(find $dir -name "$filemask"); do	
+			# cut current dir symbol
+			local filepath="${f: 1}"
+			# fetching filesize 
+			fsize=$(wc -c < "$f")
+			#echo $fsize
+			# verify on existing dir in $BKP_DIR
+			if [[ ! -d "$(dirname "$BKP_DIR$filepath")" ]]; then
+				mkdir -p "$(dirname "$BKP_DIR$filepath")"
+			fi
+
+			# verify on existing file in $BKP_DIR
+			if [[ ! -f "$BKP_DIR$filepath" ]]; then
+				cp "$f" "$BKP_DIR$filepath"
+			fi
+
+			local matched=false
+		
+			#echo $prefix
+			case $prefix in
+				-)
+					if [[ "$fsize" -lt "$numSz" ]]; then
+						matched=true							
+					fi
+					;;
+				+)
+					if [[ "$fsize" -gt "$numSz" ]]; then
+						matched=true						
+					fi
+					;;
+				=)
+					if [[ "$fsize" -eq "$numSz" ]]; then
+						matched=true
+					fi
+					;;
+			esac
+
+			if [[ "$matched" = true ]]; then
+				HUNKS=$(diff "$f" "$BKP_DIR$filepath") 
+				if [ "$HUNKS" != "" ]; then
+					echo $HUNKS > "$BKP_DIR$filepath".$(date +%s).patch
+				fi
+			fi
+		done
+		#unset IFS; set +f
 	done
 	echo "#### Done ####"
 }
